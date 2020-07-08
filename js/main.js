969b3eb4ee44b0e00a00abd17f5637b073a80c0d
cantwell <rjazantsev@gmail.com>
2020-07-08 13:51:26 +0300

    home work - day1

--- js/main.js
+++ js/main.js
@@ -6,14 +6,42 @@ const btnAddAd = document.querySelector('.add__ad'),
     modal = document.querySelector('.modal'),
     modalBtnSubmit = document.querySelector('.modal__btn-submit');
 
+//По кнопке открываем модальное окно с добавление товара
 btnAddAd.addEventListener('click', event => {
     modalAdd.classList.remove('hide');
     modalBtnSubmit.disabled = true;
 });
 
-modal.addEventListener('click', event => {
+//Зыкрываем модальное окно с добавлением товара
+modalAdd.addEventListener('click', event => {
     const target = event.target;
 
-    if (target.classList.contains('modal__close') || target.classList.contains('modal'))
-        modal.classList.add('hide');
+    if (target.classList.contains('modal__close') || target == modalAdd)
+        modalAdd.classList.add('hide');
+})
+
+//Открываем модальное окно с товаром
+catalog.addEventListener('click', event => {
+    const target = event.target;
+
+    if (target.closest('.catalog'))
+        modalItem.classList.remove('hide');
+})
+
+//Закрываем модальное окно с товаром
+modalItem.addEventListener('click', event => {
+    const target = event.target;
+
+    if (target.classList.contains('modal__close') || target == modalItem)
+        modalItem.classList.add('hide');
+})
+
+//Закрываем модальные окна по нажатию Esc
+document.addEventListener('keydown', event => {
+    if (event.keyCode == 27) {
+        const modals = document.querySelectorAll('.modal');
+        modals.forEach(modal => {
+            modal.classList.add('hide');
+        })
+    }
 })
